5b2eb1598ce861cfc190ec1a6027acd62223c754
Eduardo Preto <eduardopreto@Eduardos-MacBook-Pro-2.local>
2017-04-27 23:56:21 +0100

    add files

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title> <?php echo get_bloginfo('name'); ?></title>
+
+  <!-- Mobile Metas -->
+  <meta name= "viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Google Fonts  -->
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet' type='text/css'> <!-- Body font -->
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'> <!-- Navbar font -->
+
+  <!-- Libs and Plugins CSS -->
+  <link rel="stylesheet" href="<?php echo get_home_url(); ?>/wp-content/themes/website/include/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo get_home_url(); ?>/wp-content/themes/website/include/animations/css/animate.min.css">
+  <link rel="stylesheet" href="<?php echo get_home_url(); ?>/wp-content/themes/website/include/font-awesome/css/font-awesome.min.css"> <!-- Font Icons -->
+
+  <!-- Theme CSS -->
+  <link rel="stylesheet" href="<?php echo get_home_url(); ?>/wp-content/themes/website/css/reset.css">
+  <link rel="stylesheet" href="<?php echo get_home_url(); ?>/wp-content/themes/website/css/style.css">
+  <link rel="stylesheet" href="<?php echo get_home_url(); ?>/wp-content/themes/website/css/mobile.css">
+
+  <!-- Main CSS -->
+  <link rel="stylesheet" href="<?php echo get_home_url(); ?>/wp-content/themes/website/css/sac_theme.css">
+
+</head>
+<body>
+  <?php get_header( 'home' );?>
+</body>
