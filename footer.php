4319a7f88cac5151e0ba145e4295d7632c1eb63a
Eduardo Preto <eduardopreto@Eduardos-MacBook-Pro-2.local>
2017-04-30 06:20:45 +0100

    fix responsive footer

--- footer.php
+++ footer.php
@@ -7,23 +7,21 @@
     <div class="contact wow bounceInRight" data-wow-delay="0.4s">
         <div class="container">
           <div class="row">
-              <div class="col-sm-4">
+              <div class="col-sm-5">
                 <div class="row">
-                  <div class= "col-sm-3">
+                  <div class= "col-sm-3 col-md-3">
                     <p style = "color: #ffd83b;">Endereço</p>
                   </div>
-                  <div class= "col-sm-9" style = "border-left: 2px solid white;">
-                    <p> Departamento de Física da Universidade de Coimbra </p>
+                  <div class= "col-sm-9 col-md-3" style = "border-left: 2px solid white;">
+                    <p> Departamento de Física da </p>
+                    <p>Universidade de Coimbra </p>
                     <p> R. Larga, 3004-516 Coimbra </p>
                   </div>
                 </div>
               </div>
-
-              <div class="col-sm-4">
-
-              </div>
-              <div class="col-sm-4">
-                <a href="https://www.facebook.com/saccoimbra/?fref=ts" title="Facebook" target="_blank"><i class="fa fa-facebook fa-2x" style = "color: white; padding-right: 10px;"></i></a>
+              <div class="col-sm-4"> </div>
+              <div class="col-sm-3">
+                <a href="https://www.facebook.com/saccoimbra/?fref=ts" title="Facebook" target="_blank"><i class="fa fa-facebook fa-2x" style = "color: white; padding-right: 25px;"></i></a>
                 <a href="https://www.instagram.com/" title="Instagram" target="_blank"><i class="fa fa-instagram fa-2x" style = "color: white;"></i></a>
             </div> <!-- /.row -->
         </div> <!-- /.container -->
